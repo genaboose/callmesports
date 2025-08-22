fb45e36be3e836fc7fa1151dfa35c91575e92133
genaboose <georgnauerz@gmail.com>
2025-08-22 13:13:10 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         {/* Background image */}
         <div className="absolute inset-0">
           <img
-            src="/hero.jpg"
+            src="/public/hero.jpg"
             alt="Athlete with"
             className="w-full h-full object-cover"
           />
